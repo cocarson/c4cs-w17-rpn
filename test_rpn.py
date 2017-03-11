27705dd9935ba5a6e7b821bbb0c10d5246089222
Carson Covell <cocarson@umich.edu>
2017-03-11 15:39:12 -0500

    exp test

--- test_rpn.py
+++ test_rpn.py
@@ -8,4 +8,7 @@ class TestBasics(unittest.TestCase):
     def test_subtract(self):
         result = rpn.calculate('5 3 -')
         self.assertEqual(2, result)
+    def test_exponential(self):
+        result = rpn.calculate('2 3 ^')
+        self.assertEqual(8, result)
 
