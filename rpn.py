0f2a6a5af9d055c6e871921976ffe3bc209a54bb
Carson Covell <cocarson@umich.edu>
2017-03-24 11:44:53 -0400

    stuff

--- rpn.py
+++ rpn.py
@@ -1,5 +1,7 @@
 
 #!/usr/local/bin python3
+import sys
+from termcolor import colored
 
 import operator
 
@@ -22,15 +24,14 @@ def calculate(arg):
             arg1 = stack.pop()
             operator_fn = OPERATORS[operand]
             result = operator_fn(arg1, arg2)
-            
+
             stack.append(result)
     return stack.pop()
 
 def main():
     while True:
         result = calculate(input('rpn calc> '))
-        print("Result:", result)
+        print("Result:", colored(result, 'red', attrs=['bold']))
 
 if __name__ == '__main__':
     main()
-
